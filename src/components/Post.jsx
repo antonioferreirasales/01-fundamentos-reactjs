09be085bc6e0cce9fba928f0223c1326d78048b3
Antônio Sales <antoniosales1003@gmail.com>
2024-01-17 17:25:01 -0300

    feat: like button now works

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -29,7 +29,12 @@ export function Post({ id, author, content, publishedAt }) {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity('')
     setNewCommentText(event.target.value)
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Esse campo é obrigatório!')
   }
 
   function deleteComment(commentToDelete) {
@@ -41,6 +46,8 @@ export function Post({ id, author, content, publishedAt }) {
 
     setComments(commentsWithoutDeletedOne)
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0
 
   return (
     <article id={id} className={styles.post}>
@@ -76,10 +83,14 @@ export function Post({ id, author, content, publishedAt }) {
           placeholder="Deixe um comentário"
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
         </footer>
       </form>
       <div className={styles.commentList}>
