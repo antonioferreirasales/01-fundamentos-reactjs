10c993035d88fd2995b3f0d1e137856f6c488e2c
Antônio Sales <antoniosales1003@gmail.com>
2024-01-17 16:21:33 -0300

    fix: add keys to react lists

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,7 @@ import { ThumbsUp, Trash } from "phosphor-react"
 import styles from './Comment.module.css'
 import { Avatar } from "./Avatar"
 
-export function Comment() {
+export function Comment({ content }) {
   return (
     <div className={styles.comment}>
           <Avatar hasBorder={false} src="https://github.com/diego3g.png" />
@@ -18,7 +18,7 @@ export function Comment() {
                   <Trash size={24}/>
                 </button>
               </header>
-              <p>Muito bom Devon, parabéns!! 👏👏</p>
+              <p>{content}</p>
             </div>
             <footer>
               <button>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -6,7 +6,9 @@ import styles from './Post.module.css'
 import { useState } from 'react'
 
 export function Post({ id, author, content, publishedAt }) {
-  const [comments, setComments] = useState([1,2])
+  const [comments, setComments] = useState(['Post muito bacana, hein?!'])
+
+  const [newCommentText, setNewCommentText] = useState('')
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR
@@ -19,8 +21,15 @@ export function Post({ id, author, content, publishedAt }) {
 
   function handleCreateNewComment() {
     event.preventDefault()
+    const newContentText = event.target.comment.value
 
-    setComments([...comments, comments.length + 1])
+    setComments([...comments, newContentText])
+
+    setNewCommentText('')
+  }
+
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value)
   }
 
   return (
@@ -42,9 +51,9 @@ export function Post({ id, author, content, publishedAt }) {
       <div className={styles.content}>
         {content.map((line) => {
           if (line.type === 'paragraph') {
-            return <p>{line.content}</p>
+            return <p key={line.content}>{line.content}</p>
           } else if (line.type === 'link') {
-            return <p><a href="#">{line.content}</a></p>
+            return <p key={line.content}><a href="#">{line.content}</a></p>
           }
         })}
       </div>
@@ -52,16 +61,21 @@ export function Post({ id, author, content, publishedAt }) {
       <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
-        <textarea placeholder="Deixe um comentário"/>
+        <textarea 
+          name='comment'
+          placeholder="Deixe um comentário"
+          value={newCommentText}
+          onChange={handleNewCommentChange}
+        />
 
         <footer>
           <button type="submit">Publicar</button>
         </footer>
       </form>
       <div className={styles.commentList}>
-        {comments.map(() => {
+        {comments.map((comment) => {
           return (
-            <Comment />
+            <Comment key={comment} content={comment}/>
           )
         })}
       </div>
